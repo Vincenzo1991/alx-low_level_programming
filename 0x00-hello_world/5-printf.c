06f85a9cfd9984137384d4c2346394f2a577685f
Vincenzo1991 <ugochukwuchike@gmail.com>
2023-02-17 23:39:49 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," 
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
  * Return: Always0.
  */
 
