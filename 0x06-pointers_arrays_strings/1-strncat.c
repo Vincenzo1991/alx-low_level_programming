9fb8a610ea8fae4b05c797a090cdbdcd3602325c
Vincenzo1991 <ugochukwuchike@gmail.com>
2023-03-01 15:55:45 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,10 +2,15 @@
 
 /**
  * _strncat - Concatenates two strings using at most
+ *
  * an inputted number of bytes from src.
+ *
  * @dest: The string to be appended upon.
+ *
  * @src: The string to be appended to dest.
+ *
  * @n: The number of bytes from src to be appended to dest.
+ *
  * Return: A pointer to the resulting string dest.
  */
 
